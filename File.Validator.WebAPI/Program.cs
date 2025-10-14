f57b3b8aef346f69d90c97fba0dacb5a8e832d85
Joovs <jovannaescogar9@gmail.com>
2025-10-14 15:25:36 -0600

    mod(servicios): Agregando servicios de aplicación y módulos al programa

--- File.Validator.WebAPI/Program.cs
+++ File.Validator.WebAPI/Program.cs
@@ -1,8 +1,12 @@
+using File.Validator.Presentation.Modules;
+using File.Validator.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
 
 var app = builder.Build();
 
@@ -15,6 +19,8 @@ if (app.Environment.IsDevelopment())
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "Customer API V1");
     });
 }
+
+ModulesConfiguration.Configure(app);
 
 app.UseHttpsRedirection();
 
