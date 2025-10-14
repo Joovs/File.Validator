c105b298399cd2a4c69fd4c1fb31034321768b22
Joovs <jovannaescogar9@gmail.com>
2025-10-14 15:22:17 -0600

    Add(clase): Agregando clase request

---
+++ File.Validator.Application/Documents/Commands/ValidateFile/ValidateFileCommandRequest.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace File.Validator.Application.Documents.Commands.ValidateFile;
+
+public class ValidateFileCommandRequest
+{
+    public int UserID { get; set; }
+    public IFormFile? File { get; set; }
+}
