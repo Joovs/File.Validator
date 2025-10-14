b60bf0f3a9e731918fac97b0e68d09ccc11feea6
Joovs <jovannaescogar9@gmail.com>
2025-10-14 15:24:13 -0600

    Add(Módulo): Agregando DocumentModules

---
+++ File.Validator.Presentation/Modules/DocumentModules.cs
@@ -0,0 +1,39 @@
+﻿using File.Validator.Application.Documents.Commands.ValidateFile;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace File.Validator.Presentation.Modules;
+
+public static class DocumentModules
+{
+    private const string BASE_URL = "api/v1/document/";
+    public static void AddUserModules(this IEndpointRouteBuilder app)
+    {
+        var customerGroup = app.MapGroup(BASE_URL);
+
+        customerGroup.MapPost("", ValidateFile);
+    }
+
+    private static async Task<IResult> ValidateFile(
+        [FromForm] ValidateFileCommandRequest request,
+        ISender sender,
+        CancellationToken cancellationToken)
+    {
+        var command = new ValidateFileCommand(request);
+        var result = await sender.Send(command, cancellationToken);
+
+        if (!result.IsSuccess)
+        {
+            return Results.Problem(
+                detail: result.Error?.ErrorMessage ?? result.Message,
+                statusCode: result.StatusCode ?? 400,
+                title: result.Error?.ErrorCode
+            );
+        }
+
+        return Results.Created($"{BASE_URL}{result.Value.Id}", result.Value);
+    }
+}
