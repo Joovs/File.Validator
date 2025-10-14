ed8f9216b125add31d057905e91e5ee727f1a008
Joovs <jovannaescogar9@gmail.com>
2025-10-14 15:22:54 -0600

    Add(clase): Agregando clase sellada del command

---
+++ File.Validator.Application/Documents/Commands/ValidateFile/ValidateFileCommand.cs
@@ -0,0 +1,8 @@
+﻿using File.Validator.Domain.Shared;
+using MediatR;
+
+namespace File.Validator.Application.Documents.Commands.ValidateFile;
+
+public sealed record ValidateFileCommand (ValidateFileCommandRequest request) : IRequest<Result<ValidateFileCommandResponse>>
+{
+}
