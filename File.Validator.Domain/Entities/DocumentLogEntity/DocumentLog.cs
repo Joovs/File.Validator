c96cdad2a5c5e41069f9168074ece15594dc0497
Joovs <jovannaescogar9@gmail.com>
2025-10-14 15:21:34 -0600

    add(modelo): Agregando modelo DocumentLog

---
+++ File.Validator.Domain/Entities/DocumentLogEntity/DocumentLog.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace File.Validator.Domain.Entities.DocumentLogEntity;
+
+public class DocumentLog
+{
+    [Key]
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string FileName { get; set; } = string.Empty;
+    public bool Status { get; set; }
+    public string Description { get; set; }  = string.Empty;
+    public DateTime UploadDate { get; set; }
+}
