ac60e0f329f5835a404c1fa90f530c36f01a96cb
Joovs <jovannaescogar9@gmail.com>
2025-10-14 15:37:41 -0600

    add(contexto): Agregando contexto de la Base de Datos

---
+++ File.Validator.Infrastructure/Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using File.Validator.Domain.Entities.DocumentEntity;
+using File.Validator.Domain.Entities.DocumentLogEntity;
+using Microsoft.EntityFrameworkCore;
+
+namespace File.Validator.Infrastructure.Persistence.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    public virtual DbSet<Document> Documents { get; set; }
+    public virtual DbSet<DocumentLog> DocumentsLog { get; set; }
+
+}
