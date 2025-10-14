50fb3b4699771ddfbf66d54e285993e807e182f9
Joovs <jovannaescogar9@gmail.com>
2025-10-14 16:58:02 -0600

    add(repository): Agregando repositorios de Document y DocumentLog

---
+++ File.Validator.Infrastructure/Persistence/Repositories/DocumentLogRepository.cs
@@ -0,0 +1,43 @@
+﻿using File.Validator.Domain.Entities.DocumentLogEntity;
+using File.Validator.Domain.Entities.DocumentLogEntity.Models;
+using File.Validator.Domain.Entities.DocumentLogEntity.Repositories;
+using File.Validator.Infrastructure.Persistence.Context;
+
+namespace File.Validator.Infrastructure.Persistence.Repositories;
+
+public class DocumentLogRepository(ApplicationDbContext context) : IDocumentLogRepository
+{
+    public async Task<DocumentLogModel> RegisterLog(DocumentLogRequestModel request, CancellationToken cancellationToken)
+    {
+        DocumentLog newDocumentLog = new DocumentLog
+        {
+            UserId = request.UserId,
+            FileName = request.FileName,
+            Status = request.Status,
+            Description = request.Description,
+            UploadDate = DateTime.Now,
+        };
+
+        try
+        {
+            await context.DocumentsLog.AddAsync(newDocumentLog, cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
+        }
+        catch
+        {
+            throw new Exception();
+        }
+
+        DocumentLogModel documentModel = new DocumentLogModel
+        {
+            Id = newDocumentLog.Id,
+            UserId = newDocumentLog.UserId,
+            FileName = newDocumentLog.FileName,
+            Status = newDocumentLog.Status,
+            Description = newDocumentLog.Description,
+            UploadDate = newDocumentLog.UploadDate,
+        };
+
+        return documentModel;
+    }
+}
---
+++ File.Validator.Infrastructure/Persistence/Repositories/DocumentRepository.cs
@@ -0,0 +1,39 @@
+﻿using File.Validator.Domain.Entities.DocumentEntity;
+using File.Validator.Domain.Entities.DocumentEntity.Models;
+using File.Validator.Domain.Entities.DocumentEntity.Repositories;
+using File.Validator.Infrastructure.Persistence.Context;
+
+namespace File.Validator.Infrastructure.Persistence.Repositories;
+
+public class DocumentRepository(ApplicationDbContext context) : IDocumentRepository
+{
+    public async Task<DocumentModel> RegisterFile(DocumentRequestModel request, CancellationToken cancellationToken)
+    {
+        Document newDocument = new Document
+        {
+            UserId = request.UserId,
+            Path = request.Path,
+            UploadDate = DateTime.Now,
+        };
+
+        try
+        {
+            await context.Documents.AddAsync(newDocument, cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
+        }
+        catch
+        {
+            throw new Exception();
+        }
+
+        DocumentModel docuementModel = new DocumentModel
+        {
+            Id = newDocument.Id,
+            UserId = newDocument.UserId,
+            Path = newDocument.Path,
+            UploadDate = newDocument.UploadDate,
+        };
+
+        return docuementModel;
+    }
+}
