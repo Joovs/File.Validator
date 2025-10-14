3fea8bb9316d75ab9c814581687fd8a4f09ade32
Joovs <jovannaescogar9@gmail.com>
2025-10-14 15:23:15 -0600

    Add(clase): Agregando clase handler vacío

---
+++ File.Validator.Application/Documents/Commands/ValidateFile/ValidateFileCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using File.Validator.Domain.Shared;
+using MediatR;
+
+namespace File.Validator.Application.Documents.Commands.ValidateFile;
+
+public class ValidateFileCommandHandler : IRequestHandler<ValidateFileCommand, Result<ValidateFileCommandResponse>>
+{
+    public Task<Result<ValidateFileCommandResponse>> Handle(ValidateFileCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
