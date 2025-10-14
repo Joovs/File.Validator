cf8621c7aa79c659fa9023f6578b56fe3d6825d2
Joovs <jovannaescogar9@gmail.com>
2025-10-14 15:25:00 -0600

    add(Configuración de módulos): Agregando ModulesConfiguration

---
+++ File.Validator.Presentation/Modules/ModulesConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace File.Validator.Presentation.Modules;
+
+public class ModulesConfiguration
+{
+    public static void Configure(WebApplication app)
+    {
+        app.AddUserModules();
+    }
+}
