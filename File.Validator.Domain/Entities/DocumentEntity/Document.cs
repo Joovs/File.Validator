16102b67691d6461b64cc5d4ede5c3bf9687c4b2
Joovs <jovannaescogar9@gmail.com>
2025-10-14 15:21:12 -0600

    add(modelo): Agregando modelo Document

---
+++ File.Validator.Domain/Entities/DocumentEntity/Document.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace File.Validator.Domain.Entities.DocumentEntity;
+
+public class Document
+{
+    [Key]
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public string Path { get; set; } = string.Empty;
+    public DateTime UploadDate { get; set; }
+}
