d4d5da6f2e42c6d27941bda889a5fa3cc591394b
Joovs <jovannaescogar9@gmail.com>
2025-10-14 23:34:25 -0600

    mod(model): Eliminando campo Id de modelo

--- File.Validator.Domain/Entities/DocumentLogEntity/Models/DocumentLogRequestModel.cs
+++ File.Validator.Domain/Entities/DocumentLogEntity/Models/DocumentLogRequestModel.cs
@@ -2,7 +2,6 @@
 
 public class DocumentLogRequestModel
 {
-    public int Id { get; set; }
     public int UserId { get; set; }
     public string FileName { get; set; } = string.Empty;
     public bool Status { get; set; }
