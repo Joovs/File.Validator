c08ea72cab896486ffbc453cb3c42ff890870990
Joovs <jovannaescogar9@gmail.com>
2025-10-14 23:33:22 -0600

    add(manager): Agregando gestor de servicios

---
+++ File.Validator.Infrastructure/InjectionManagers/ServicesManager.cs
@@ -0,0 +1,18 @@
+﻿using File.Validator.Domain.Services.FileSaver;
+using File.Validator.Domain.Services.VirusScanner;
+using File.Validator.Infrastructure.Services.FileSaver;
+using File.Validator.Infrastructure.Services.VirusScanner;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace File.Validator.Infrastructure.InjectionManagers;
+
+public static class ServicesManager
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddTransient<IVirusScanner, VirusScanner> ();
+        services.AddTransient<IFileSaver, FileSaver>();
+
+        return services;
+    }
+}
