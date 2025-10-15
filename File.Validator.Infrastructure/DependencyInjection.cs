57f199a9be09314b44c02bb0b2edbb5d7b8a929e
Joovs <jovannaescogar9@gmail.com>
2025-10-14 18:19:16 -0600

    add(dependencies): Agregando manejo de inyección de dependencias

---
+++ File.Validator.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using File.Validator.Infrastructure.InjectionManagers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace File.Validator.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        RepositoriesManager.AddRepositories(services);
+
+        return services;
+    }
+}
---
+++ File.Validator.Infrastructure/InjectionManagers/RepositoriesManager.cs
@@ -0,0 +1,17 @@
+﻿using File.Validator.Domain.Entities.DocumentEntity.Repositories;
+using File.Validator.Domain.Entities.DocumentLogEntity.Repositories;
+using File.Validator.Infrastructure.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace File.Validator.Infrastructure.InjectionManagers;
+
+public static class RepositoriesManager
+{
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddTransient<IDocumentRepository, DocumentRepository>();
+        services.AddTransient<IDocumentLogRepository, DocumentLogRepository>();
+
+        return services;
+    }
+}
