8cd458cc4a7bc0ef289ea3086c018ee5a968c91a
Joovs <jovannaescogar9@gmail.com>
2025-10-14 23:32:36 -0600

    add(services): Agregando servicios

---
+++ File.Validator.Infrastructure/Services/FileSaver/FileSaver.cs
@@ -0,0 +1,27 @@
+﻿using File.Validator.Domain.Services.FileSaver;
+using Microsoft.AspNetCore.Http;
+
+namespace File.Validator.Infrastructure.Services.FileSaver;
+
+public class FileSaver : IFileSaver
+{
+    public async Task<string> SaveFileAsync(IFormFile file)
+    {
+        string path = Path.Combine("Uploads", file.FileName);
+
+        Directory.CreateDirectory("Uploads");
+
+        var stream = new FileStream(path, FileMode.Create);
+
+        try
+        {
+            await file.CopyToAsync(stream);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(ex.Message);
+        }
+
+        return path;
+    }
+}
---
+++ File.Validator.Infrastructure/Services/VirusScanner/VirusScanner.cs
@@ -0,0 +1,52 @@
+﻿using File.Validator.Domain.Services.VirusScanner;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using VirusTotalNet;
+
+namespace File.Validator.Infrastructure.Services.VirusScanner;
+
+public class VirusScanner : IVirusScanner
+{
+    private readonly VirusTotal _virusTotal;
+
+    public VirusScanner(IConfiguration configuration)
+    {
+        var apiKey = configuration["VirusTotal:ApiKey"];
+
+        _virusTotal = new VirusTotal(apiKey)
+        {
+            UseTLS = true
+        };
+    }
+
+    public async Task<bool> isSafe(IFormFile file)
+    {
+        try
+        {
+            // para que los recursos descartables se eliminen correctamente
+            using (MemoryStream ms = new MemoryStream())
+            {
+                await file.CopyToAsync(ms);
+                ms.Position = 0;
+
+                var scanResult = await _virusTotal.ScanFileAsync(ms, file.FileName);
+                if (scanResult == null || string.IsNullOrEmpty(scanResult.Resource)) return false;
+
+                var report = await _virusTotal.GetFileReportAsync(scanResult.Resource);
+                if (report == null) return false;
+
+                if (report.ResponseCode.ToString().Equals("Present", StringComparison.OrdinalIgnoreCase) || report.ResponseCode.ToString() == "1")
+                {
+                    return report.Positives == 0;
+                }
+
+                return false;
+            }
+        }
+        catch (Exception ex)
+        { 
+            throw new Exception(ex.Message);
+        }
+        
+    }
+}
